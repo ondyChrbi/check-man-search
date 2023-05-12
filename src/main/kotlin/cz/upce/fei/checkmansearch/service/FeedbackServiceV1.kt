d68ed7304e1f1b1165500c27777038106b4bece8
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-05-12 19:35:48 +0200

    Design changes

--- src/main/kotlin/cz/upce/fei/checkmansearch/service/FeedbackServiceV1.kt
+++ src/main/kotlin/cz/upce/fei/checkmansearch/service/FeedbackServiceV1.kt
@@ -1,33 +1,28 @@
 package cz.upce.fei.checkmansearch.service
 
 import cz.upce.fei.checkmansearch.domain.Feedback
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.QueryBuilders
 import org.springframework.data.domain.Pageable
-import org.springframework.data.elasticsearch.client.reactive.ReactiveElasticsearchClient
 import org.springframework.data.elasticsearch.core.ReactiveElasticsearchOperations
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
+import java.util.*
 
 @Service
 class FeedbackServiceV1(
-    private val reactiveElasticsearchOperations: ReactiveElasticsearchOperations,
-    private val reactiveElasticsearchClient : ReactiveElasticsearchClient
+    private val reactiveElasticsearchOperations: ReactiveElasticsearchOperations
 ) {
     fun findAll(description: String, courseId: Long?, authorId: Long?): Flux<Feedback> {
         val query =  NativeSearchQueryBuilder()
 
         if (description.isNotEmpty()) {
-            query.withQuery(QueryBuilders.prefixQuery("description", description))
+            query.withQuery(QueryBuilders.prefixQuery("description", description.lowercase(Locale.getDefault())))
         }
 
         if (courseId != null) {
             query.withQuery(QueryBuilders.matchQuery("courseId", courseId.toString()))
-        }
-
-        if (authorId != null) {
-
         }
 
         query.withPageable(Pageable.ofSize(PAGE_SIZE))
